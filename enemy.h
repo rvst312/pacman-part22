916536a3a8d13e16cda52c8bb69b9280844805dd
rvst312 <115166784+rvst312@users.noreply.github.com>
2022-12-26 12:40:59 +0100

    new

--- enemy.h
+++ enemy.h
@@ -8,6 +8,18 @@
 
 class Enemy
 {
+public:
+    COORD position;
+private:
+    char character = 'E'
+    ConsoleUtils::CONSOLE_COLOR foreground = ConsoleUtils::CONSOLE_COLOR::DARK_RED
+    ConsoleUtils::CONSOLE_COLOR background = ConsoleUtils::CONSOLE_COLOR::BLACK
+    
+public:
+    Enemy(COORD spawn);
+    
+    void Logic(Map* pacman_map);
+    void Draw();
     
 };
 
